86827055df5e85b4a0a27da6d935210f6f47b819
Lital Josifov <litalj@wobi.co.il>
2018-06-13 15:17:54 +0300

    Added buttons to form

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -18,6 +18,7 @@ export class PostCreateComponent {
       return;
     }
 
-    this.postService.addpost(form.value.title, form.value.content)
+    this.postService.addpost(form.value.title, form.value.content);
+    form.resetForm();
   }
 }
